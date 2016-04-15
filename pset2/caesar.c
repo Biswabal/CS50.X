549b73b7d6e462cfb4fd6595d33ae35d33f84065
Biswabal Gurung <grg2biswa@gmail.com>
2016-04-15 15:31:06 +0530

    Update caesar.c

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,8 +1,8 @@
 /**
- * vigenere.c
- *
- * Encrypts text using vigenere
- *
+ * caeser.c
+ * 
+ * Encrypts text using caeser cipher method
+ * 
  */
  
  #include <stdio.h>
@@ -13,61 +13,44 @@
  
  int main(int argc, string argv[])
  {
-    if (argc!=2)
-    {
+     int k;
+     if (argc != 2)
+     {
         printf("Enter correct input\n");
         return 1;
-    }
-    else
-    {
-        string key = argv[1];
-        int k_len = strlen(key);
-        int count=0;
-        //checks all the characters of key are alphabet
-        for (int i = 0;i < k_len; i++)
-        {
-            if (isalpha(key[i]))
-                continue;
-            else
-            {
-                count=1;
-                break;
-            }
-        }
-        if(count!=0)
-        {
-            printf("Enter correct input\n");
-            return 1;
-        }
-        else
-        {
+     }
+     else 
+     {
+         k=atoi(argv[1]);
+         if (k >= 0)
+         {
             string message = GetString();
-            int cipher,k;
-            int j=0;
             for (int i=0, n = strlen(message); i < n; i++)
             {
-                if (isupper(message[i]))
-                {
-                    k = ((int)(toupper(key[(j% k_len)]))-'A');
+                 int cipher;
+                 k = k % 26;
+                 if (isupper(message[i]))
+                 {
+                     //formula to encrypt uppercase alphabets
                     cipher = (int)message[i] + k;
                     if(cipher > 90)
-                        cipher = 64 + cipher - 90;
-                    j++;
-                }
+                    cipher = 64 + cipher - 90;
+                 }
                 else if (islower(message[i]))
                 {
-                    k = (int)(toupper(key[(j % k_len)]))-'A';
+                    //formula to encrypt lowercase alphabets
                     cipher = (int)message[i] + k;
                     if (cipher > 122)
-                        cipher = 96 + cipher - 122;
-                    j++;
+                    cipher = 96 + cipher - 122;
                 }
                 else
-                    cipher = (int)message[i];
+                     cipher = (int)message[i];
                 printf("%c",(char)cipher);
             }
-            printf("\n");
-            return 0;
+         printf("\n");
+         return 0;
         }
+        else
+            return 1;
     }
- }
+}
